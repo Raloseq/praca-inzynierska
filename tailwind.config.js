f105fbe8377a25a6444312bdbb43c4257abfb587
Rafał Brzeziński <rbrzezinski@sevroll.system>
2022-09-28 13:49:25 +0200

    created front page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            backgroundImage: {
+                'hero-pattern': "url('./storage/app/images/front-page.jpg')",
+            },
         },
     },
 
