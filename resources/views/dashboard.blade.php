30d97d140e6f53999f95df2f3c065723d1346b03
Rafał Brzeziński <rbrzezinski@sevroll.system>
2022-09-28 12:44:46 +0200

    created sidebar for app

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,12 +5,11 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+    <div class="h-screen">
+        <div class="flex h-full">
+            @include('components.sidebar')
+            <div class="w-4/5 h-full">
+                Main
             </div>
         </div>
     </div>
