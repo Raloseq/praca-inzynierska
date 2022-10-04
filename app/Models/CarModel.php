94e6aeea56a2bf4127d6a5e9190ce55c32bf955c
Rafał Brzeziński <rbrzezinski@sevroll.system>
2022-10-04 13:26:03 +0200

    added model,brand and type of car model and tables

---
+++ app/Models/CarModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'model';
+
+    protected $fillable = [
+        'name', 'brand_id'
+    ];
+
+    public function brands()
+    {
+        return $this->hasMany(Brand::class, 'brand_id', 'id');
+    }
+}
