e8d037730b0030011a7c5689111b6e3d7a6a02a1
Rafał Brzeziński <rbrzezinski@sevroll.system>
2022-11-02 15:03:39 +0100

    add changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserDataRequest;
+
 class UserController extends Controller
 {
     public function index()
